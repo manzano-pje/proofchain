617bc6840dca40c9852d3eb4f2069b87dacdd0ca
Paulo Manzano <93448139+manzano-pje@users.noreply.github.com>
2026-01-04 20:50:53 -0300

    feat: criação section recursos

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -14,7 +14,7 @@
     </section>   
 
     <!-- Autenticação -->
-     <section id="id-validacao" class="validacao">
+    <section id="id-validacao" class="validacao">
         <div class="validation-box">
             <h3>Autenticação de Certificados</h3>
             <p>Confirme a validade de um certificado em segundos,<br>
@@ -24,7 +24,62 @@
                 <a href="#validar" class="btn btn-outline">Validar Certificado</a>
             </div>
         </div>
-     </section>
+    </section>
+
+    <!-- Recursos -->
+    <section id="id-recursos" class="recursos">
+        <div class="container">
+            <div class="recursos-box">
+                <h3>Por que escolher o <span>ProofChain?</span></h3>
+                <p>
+                Uma plataforma completa para emissão e autenticação <br>
+                de certificados digitais com total confiança.
+                </p>
+
+
+                <div class="grid">
+                    <div class="card">
+                        <i class="fa-solid fa-shield"></i>
+                        <h3>Segurança contra fraudes</h3>
+                        <p>
+                        Cada certificado é registrado em blockchain,
+                        tornando qualquer tentativa de alteração
+                        impossível.
+                        </p>
+                    </div>
+
+                    <div class="card">
+                        <i class="fa-solid fa-fingerprint"></i>
+                        <h3>Autenticidade garantida</h3>
+                        <p>
+                        Qualquer pessoa pode verificar a
+                        autenticidade de um certificado
+                        em poucos segundos.
+                        </p>
+                    </div>
+
+                    <div class="card">
+                        <i class="fa-solid fa-users"></i>
+                        <h3>Acesso público e institucional</h3>
+                        <p>  
+                        Visitantes validam certificados
+                        livremente, enquanto instituições
+                        gerenciam emissões com segurança.
+                        </p>
+                    </div>
+
+                    <div class="card">
+                        <i class="fa-solid fa-certificate"></i>
+                        <h3>Identidade visual personalizada</h3>
+                        <p>
+                        Personalize certificados com logotipo,
+                        cores e layout da sua instituição.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 </template>
 
 <script setup lang="ts">
