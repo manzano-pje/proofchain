1c1775ff2bf383a2ba130732f590ee8a96b7b535
Paulo Manzano <93448139+manzano-pje@users.noreply.github.com>
2026-01-30 15:53:43 -0300

    troca de bransh

--- backend/proofchain/src/main/java/com/proofchain/controller/UserController.java
+++ backend/proofchain/src/main/java/com/proofchain/controller/UserController.java
@@ -1,7 +1,12 @@
 package com.proofchain.controller;
 
+import com.proofchain.Dtos.UserRequestDto;
 import com.proofchain.service.UserService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,10 +16,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/user")
 public class UserController {
 
-//    private final UserService userService;
+    private final UserService userService;
 
-//    @PostMapping("/register")
-//    public ResponseEntity<String> registerUser(@RequestBody userRegisterDto user) {
-//        return userService.registerUser(user);
-//    }
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @PostMapping("/register")
+    public ResponseEntity<String> registerUser(@RequestBody UserRequestDto user) {
+        return userService.registerUser(user);
+    }
 }
--- backend/proofchain/src/main/java/com/proofchain/service/UserService.java
+++ backend/proofchain/src/main/java/com/proofchain/service/UserService.java
@@ -1,13 +1,19 @@
 package com.proofchain.service;
 
+import com.proofchain.Dtos.UserRequestDto;
+import com.proofchain.repository.UserRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
 public class UserService {
 
-//    private final UserRepository userRepository;
-//
-//    public ResponseEntity<String> registerUser(userRegisterDto user) {
+    private final UserRepository userRepository;
+
+    public ResponseEntity<String> registerUser(UserRequestDto user) {
+
+        return null;
+    }
 }
