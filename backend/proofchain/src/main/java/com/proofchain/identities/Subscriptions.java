10b3a5fa5f58f44de7099953020b3f7b188f3d32
Paulo Manzano <93448139+manzano-pje@users.noreply.github.com>
2026-02-17 01:06:45 -0300

    feat: criação das tabelas plans, subscriptions, usage_counter, feature_plans

---
+++ backend/proofchain/src/main/java/com/proofchain/identities/Subscriptions.java
@@ -0,0 +1,48 @@
+package com.proofchain.identities;
+
+import com.proofchain.identities.enums.Status;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.Instant;
+
+
+/*
+ * Subscrição indica
+ * Qual plano está ativo
+ * Quando começou
+ * Quando expira
+ * Se está ativo, cancelado ou expirado
+ * Se é recorrente ou não
+ */
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "tb_subscriptions")
+public class Subscriptions {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idSubscription;
+//    private Long idInstituition;
+//    private Long idPlan;
+
+    @Enumerated(EnumType.STRING)
+    private Status status;
+    private Instant startsAt;
+    private Instant expiresAt;
+    private Instant createdAt;
+
+    @ManyToOne
+    @JoinColumn(name = "id_instituition")
+    private Instituition instituition;
+
+    @ManyToOne
+    @JoinColumn(name = "id_plan")
+    private Plans plans;
+}
---
+++ backend/proofchain/src/main/java/com/proofchain/identities/enums/Status.java
@@ -0,0 +1,7 @@
+package com.proofchain.identities.enums;
+
+public enum Status {
+    ACTIVE,
+    EXPIRED,
+    CANCELED
+}
