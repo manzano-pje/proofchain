5b257251e01607aef648d752e24ce53d5e22b35b
Paulo Manzano <93448139+manzano-pje@users.noreply.github.com>
2026-02-27 18:47:23 -0300

    acerto de projeto

--- backend/proofchain/src/main/java/com/proofchain/repository/PlansRepository.java
+++
@@ -1,9 +0,0 @@
-package com.proofchain.repository;
-
-import com.proofchain.identities.Plans;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PlansRepository extends JpaRepository<Plans, Long> {
-}
