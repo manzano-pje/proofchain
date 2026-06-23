8a3f30caa07366f34d07aaec3553927bdfc6312c
Paulo Manzano <93448139+manzano-pje@users.noreply.github.com>
2026-06-23 01:56:45 -0300

    feat: criação de SystemBootstrap

--- backend/proofchain/src/main/java/com/proofchain/plataform/domain/OpenApiConfig.java
+++ backend/proofchain/src/main/java/com/proofchain/plataform/domain/OpenApiConfig.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.context.annotation.Configuration;
 
@@ -28,5 +29,6 @@ import org.springframework.context.annotation.Configuration;
                 )
         )
 )
+
 public class OpenApiConfig {
 }
--- backend/proofchain/src/main/java/com/proofchain/user/infrastructure/repository/UserRepository.java
+++ backend/proofchain/src/main/java/com/proofchain/user/infrastructure/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.proofchain.user.infrastructure.repository;
 
 import com.proofchain.user.domain.model.User;
+import com.proofchain.user.domain.model.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -13,9 +14,11 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmailAndInstitution_Id(String email, Long id);
     Optional<User> findByIdAndInstitution_Id(Long id, Long InstitutionId);
     Optional<User> findByEmail(String email);
+    List<User> findAllByInstitution_IdAndInstitution_DeletedAtIsNull(Long institutionId);
+
     boolean existsByIdAndInstitutionId(Long id, Long institutionId);
     boolean existsByNameAndInstitutionId(String name, Long institutionId);
     boolean existsByEmail(String email);
+    boolean existsByRole(UserRole userRole);
 
-    List<User> findAllByInstitution_IdAndInstitution_DeletedAtIsNull(Long institutionId);
     }
