11171287748e63e945d83ed3488deaab07ace3ed
Paulo Manzano <93448139+manzano-pje@users.noreply.github.com>
2026-07-20 17:47:03 -0300

    feat: criação de mensagens de excessão centralizadas

---
+++ backend/proofchain/src/main/java/com/proofchain/shared/exception/messages/InstitutionMessages.java
@@ -0,0 +1,10 @@
+package com.proofchain.shared.exception.messages;
+
+public class InstitutionMessages {
+
+    private InstitutionMessages(){}
+
+    public static final String INSTITUTION_ALREADY_EXISTS = "Instituição já cadastrada.";
+    public static final String INSTITUTION_NOT_FOUND = "Instituição não cadastrada.";
+    public static final String INSTITUTION_NOT_AUTORIZED = "Instituição não autorizada.";
+}
---
+++ backend/proofchain/src/main/java/com/proofchain/shared/exception/messages/SubscriptionMessages.java
@@ -0,0 +1,10 @@
+package com.proofchain.shared.exception.messages;
+
+public class SubscriptionMessages {
+
+    private SubscriptionMessages(){}
+
+    public static final String SUBSCRIPTION_NOT_FOUND = "Assinatura não cadastrada.";
+    public static final String SUBSCRIPTION_ALREADY_EXISTS = "Assinatura já cadastrada.";
+    public static final String SUBSCRIPTION_INACTIVE = "Assinatura inativa.";
+}
