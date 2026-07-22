f6afca2e2b18ca156033ba7631afeb5afb2113b3
Paulo Manzano <93448139+manzano-pje@users.noreply.github.com>
2026-07-22 20:35:29 -0300

    feat: criação InstructorController

---
+++ backend/proofchain/src/main/java/com/proofchain/business/instructor/application/command/UpdateInstructorCommand.java
@@ -0,0 +1,24 @@
+package com.proofchain.business.instructor.application.command;
+
+import com.proofchain.business.instructor.interfaces.dto.request.UpdateInstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Component;
+
+import java.time.Instant;
+
+@Component
+@Setter
+@Getter
+public class UpdateInstructorCommand {
+
+    private String specialty;
+    private Instant hiringDate;
+    private boolean isActive;
+
+    public UpdateInstructorCommand(UpdateInstructor dto) {
+        this.specialty = dto.getSpecialty();
+        this.hiringDate = dto.getHiringDate();
+        this.isActive = dto.isActive();
+    }
+}
--- backend/proofchain/src/main/java/com/proofchain/business/instructor/interfaces/controller/InstructorController.java
+++ backend/proofchain/src/main/java/com/proofchain/business/instructor/interfaces/controller/InstructorController.java
@@ -1,16 +1,15 @@
 package com.proofchain.business.instructor.interfaces.controller;
 
 import com.proofchain.business.instructor.application.command.CreateInstructorCommand;
+import com.proofchain.business.instructor.application.command.UpdateInstructorCommand;
 import com.proofchain.business.instructor.application.handler.CreateInstructorHandler;
 import com.proofchain.business.instructor.interfaces.dto.request.InstructorRequest;
+import com.proofchain.business.instructor.interfaces.dto.request.UpdateInstructor;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -23,8 +22,8 @@ public class InstructorController {
      * =========================================================
      */
       private final CreateInstructorHandler createInstructorHandler;
-//    private final ListAllInstructorHandler listAllInstructorHandler;
-//    private final ListOneInstructorHandler listOneInstructorHandler;
+      private final ListAllInstructorHandler listAllInstructorHandler;
+//    private final GetOneInstructorHandler getOneInstructorHandler;
 //    private final UpdateInstructorHandler updateInstructoHandler;
 //    private final DeleteInstructorHandler deleteInstructorHandler;
 
@@ -34,12 +33,32 @@ public class InstructorController {
      * =========================================================
      */
 
-    @PostMapping
+    @PostMapping("/register")
     @PreAuthorize("hasRole('SUPER_ADMIN','ADMIN', 'USER')")
     public ResponseEntity<Void> createInstructor(@Valid @RequestBody InstructorRequest dto){
         CreateInstructorCommand command = new CreateInstructorCommand(dto);
         createInstructorHandler.createInstructor(command);
         return ResponseEntity.ok().build();
     }
+
+    @GetMapping("/list")
+    @PreAuthorize("hasRole('SUPER_ADMIN','ADMIN', 'USER')")
+    public List<InstructorResponse> listAllInstructors(){
+        return listAllInstructorHandler.listAllInstructors();
+    }
+
+//    @GetMapping("/list/{id}")
+//    @PreAuthorize("hasRole('SUPER_ADMIN','ADMIN', 'USER')")
+//    public List<InstructorResponse> getOneInstructor(@PathVariable Long id){
+//        return getOneInstructorHandler.getOneInstructor(id);
+//    }
+
+//    @PathMapping("/update/{id}")
+//    @PreAuthorize("hasRole('SUPER_ADMIN','ADMIN', 'USER')")
+//    public ResponseEntity<Void> updateInstructor(@Valid @PathVariable Long id, @RequestBody UpdateInstructor dto){
+//        UpdateInstructorCommand command = new UpdateInstructorCommand(dto);
+//        updateInstructoHandler.updateInstructor(id, command);
+//        return ResponseEntity.ok().build();
+//    }
 
 }
