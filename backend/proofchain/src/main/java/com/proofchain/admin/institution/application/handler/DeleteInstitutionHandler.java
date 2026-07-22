3610bcc4be38334cd2ad8941eecce281ce885756
Paulo Manzano <93448139+manzano-pje@users.noreply.github.com>
2026-07-22 18:04:55 -0300

    feat: refatoração validação tenat em Institution

--- backend/proofchain/src/main/java/com/proofchain/admin/institution/application/handler/DeleteInstitutionHandler.java
+++ backend/proofchain/src/main/java/com/proofchain/admin/institution/application/handler/DeleteInstitutionHandler.java
@@ -4,23 +4,33 @@ import com.proofchain.admin.institution.domain.model.Institution;
 import com.proofchain.admin.institution.infrastructure.repository.InstitutionRepository;
 import com.proofchain.shared.exception.NotFoundException;
 import com.proofchain.shared.exception.messages.InstitutionMessages;
+import com.proofchain.shared.security.SecurityUtils;
+import com.proofchain.shared.util.TenatValidation;
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.Instant;
 
 @Component
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class DeleteInstitutionHandler {
 
     private InstitutionRepository institutionRepository;
+    private final TenatValidation tenatValidation;
 
     @Transactional
     public void deleteinstitution(String cnpj){
 
-//        Long institutionId = SecurityUtils.getInstitutionId();
-//        validations.validateinstitution(institutionId);
+        /*
+         * =========================================================
+         * CONTEXTO DE INSTITUIÇÃO (TENANT)
+         * =========================================================
+         */
+        Long institutionId = SecurityUtils.getInstitutionId();
+        tenatValidation.validateInstitution(institutionId);
+
 
         Institution institution = institutionRepository.findByCnpjAndDeletedAtIsNull(cnpj)
                 .orElseThrow(() -> new NotFoundException(InstitutionMessages.INSTITUTION_NOT_FOUND));
--- backend/proofchain/src/main/java/com/proofchain/admin/institution/application/handler/UpdateInstitutionHandler.java
+++ backend/proofchain/src/main/java/com/proofchain/admin/institution/application/handler/UpdateInstitutionHandler.java
@@ -5,19 +5,28 @@ import com.proofchain.admin.institution.infrastructure.repository.InstitutionRep
 import com.proofchain.admin.institution.interfaces.dtos.request.UpdateInstitutionRequest;
 import com.proofchain.shared.exception.NotFoundException;
 import com.proofchain.shared.exception.messages.InstitutionMessages;
-import lombok.AllArgsConstructor;
+import com.proofchain.shared.security.SecurityUtils;
+import com.proofchain.shared.util.TenatValidation;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class UpdateInstitutionHandler {
 
     private final InstitutionRepository institutionRepository;
+    private final TenatValidation tenatValidation;
 
     public void updateinstitution(String cnpj, UpdateInstitutionRequest UpdateInstitutionRequest){
 
-//        Long institutionId = SecurityUtils.getInstitutionId();
-//        validations.validateinstitution(institutionId);
+        /*
+         * =========================================================
+         * CONTEXTO DE INSTITUIÇÃO (TENANT)
+         * =========================================================
+         */
+        Long institutionId = SecurityUtils.getInstitutionId();
+        tenatValidation.validateInstitution(institutionId);
+
 
         Institution institution = institutionRepository.findByCnpjAndDeletedAtIsNull(cnpj)
                 .orElseThrow(() -> new NotFoundException(InstitutionMessages.INSTITUTION_NOT_FOUND));
