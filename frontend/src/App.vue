d6bd4d0f6c5a44683506c0e4efc4b8ba0d00377b
Paulo Manzano <93448139+manzano-pje@users.noreply.github.com>
2026-08-04 20:03:28 -0300

    feat: estruturação de pastas e arquivos base de estilios

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,28 +1,11 @@
-<script setup lang="ts">
-import { RouterView } from 'vue-router' 
-import Header from './components/layout/Header.vue'
-import Footer from './components/layout/Footer.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <div class="app-layout">
-    <Header />
-    <main>
-      <RouterView />
-    </main>
-    <Footer />
-  </div>
+  <h1>You did it!</h1>
+  <p>
+    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+    documentation
+  </p>
 </template>
 
-<style scoped>
-.app-layout {
-  display: flex;
-  flex-direction: column;
-  min-height: 100vh;
-}
-
-main {
-  width: 100%;
-  flex: 1; /* This makes the main content expand to fill available space, pushing footer down */
-}
-</style>
+<style scoped></style>
