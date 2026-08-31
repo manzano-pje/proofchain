e87ebb7375d4ad634689e208974ee6b0566a75f4
Paulo Manzano <93448139+manzano-pje@users.noreply.github.com>
2026-08-31 13:28:29 -0300

    feat/ ajuste de cores dos cards

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -12,10 +12,10 @@
 
 <script setup>
 import Header from '@/layouts/components/Header/Header.vue'
-import Footer from '@/layouts/components/Footer/Footer.vue'
 import Hero from '@/modules/landing/components/Hero/Hero.vue'
 import Audiences from './modules/landing/components/Audiences/Audiences.vue'
 import Flow from './modules/landing/components/Flow/Flow.vue'
 import CrownCarousel from './modules/landing/components/CrownCarousel/CrownCarousel.vue'
 import Pricing from './modules/landing/components/Pricing/Pricing.vue'
+import Footer from '@/layouts/components/Footer/Footer.vue'
 </script>
