6aa8f4b252b3a00cbcfd9bb35dfcfed166394756
Paulo Manzano <93448139+manzano-pje@users.noreply.github.com>
2026-09-16 19:18:12 -0300

    feat/ criação da integração login e onboarding com backend

--- backend/proofchain/src/main/java/com/proofchain/shared/security/SecurityBeansConfig.java
+++ backend/proofchain/src/main/java/com/proofchain/shared/security/SecurityBeansConfig.java
@@ -123,7 +123,8 @@ public class SecurityBeansConfig {
 
         config.setAllowedOrigins(List.of("http://localhost:8080",
                                          "http://127.0.0.1:8080",
-                                         "http://localhost:3000")
+                                         "http://localhost:3000",
+                                         "http://localhost:5173")
         ); // ou frontend
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
